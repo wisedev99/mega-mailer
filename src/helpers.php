14012e8c73efb268d48bfd518f9d9e14af18e9ba
Aliakbar Boistov <wisedev2699@gmail.com>
2024-11-15 12:10:32 +0500

    fixed

--- src/helpers.php
+++ src/helpers.php
@@ -1,5 +1,7 @@
 <?php
 
-require_once 'vendor/autoload.php';
+// Correct path to autoload.php from the helpers.php file
+require_once __DIR__ . '/../../../../vendor/autoload.php';
 
+// Ensure the class exists
 $class = new \Megafon\App\Mailer();
